d42c745d1a7837f9facf90d2cda931a37c8965f0
Squall <13702239693@139.com>
2014-12-26 18:43:27 +0800

    更新权限数据库接口

--- src/main/java/com/ruipengkj/common/dao/Condition.java
+++ src/main/java/com/ruipengkj/common/dao/Condition.java
@@ -5,7 +5,27 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Condition {
+import com.ruipengkj.common.util.NameUtils;
+
+public class Condition<T> {
+	
+	public static final String AND = " and ";//并且
+	public static final String OR = " or ";//或者
+	public static final String EQ = " = ";//等于
+	public static final String NE = " != ";//不等于
+	public static final String LT = " < ";//小于
+	public static final String GT = " > ";//等于
+	public static final String LE = " <= ";//小于等于
+	public static final String GE = " >= ";//大于等于
+	public static final String LIKE = " like ";//模糊匹配
+	public static final Object BETWEEN = " between ";
+	public static final String WHERE = " where ";//条件
+	public static final String SET = " set ";//赋值
+	public static final String ORDER_BY = " order by ";//排序
+	public static final String ASC = "asc";//升序
+	public static final String DESC = "desc";//降序
+	
+	
 	private List<String> fields = null;
 	private List<String> conditions = null;
 	private Map<String, Object> expressions = null;
@@ -17,14 +37,14 @@ public class Condition {
 		conditions = new ArrayList<String>();
 		expressions = new HashMap<String, Object>();
 	}
-	public void selectField(String field) {
-		fields.add(field);
+
+	public void selectField(String property) {
+		fields.add(NameUtils.toUnderlineName(property));
 	}
-	public void addExpression(String expression){
-		conditions.add(expression);
-	}
-	public void addParam(String key, Object value){
-		expressions.put(key, value);
+	public void addExpression(String property, String operator, Object value){
+		String name = ":" + property;
+		conditions.add(NameUtils.toUnderlineName(property) + operator + name);
+		expressions.put(name, value);
 	}
 	public void orderBy(String orderBy) {
 		this.orderBy = orderBy;
